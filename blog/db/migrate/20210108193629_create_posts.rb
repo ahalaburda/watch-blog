3432003d10261849dc5b78023f20b846e89ff67f
Adrian Halaburda <adh761@gmail.com>
2021-01-08 17:15:02 -0300

    update: Post model added

---
+++ blog/db/migrate/20210108193629_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :author
+      t.string :image_url
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
