e42ad7ef6ff3c0bb8ab255a69a8b20d8d0d88e4b
Adrian Halaburda <adh761@gmail.com>
2021-01-10 20:16:53 -0300

    update: News from local api component

---
+++ blog/app/javascript/services/Local/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import Entry from '../../components/Entry'
+
+class Local extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      articles: []
+    }
+  }
+
+  getNews() {
+     axios.get('http://localhost:3000/api/v1/posts.json')
+        .then(response => {
+          this.setState({articles: response.data.data});
+          console.log(typeof this.state.articles);
+          console.log(this.state.articles);
+
+        })
+        .catch(error => console.log(error))
+  }
+
+  componentDidMount(){
+   this.getNews();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Local News.</h2>
+        <div>
+          {this.state.articles.map( article  => {
+            return (
+              <Entry  id={article.attributes.id}
+                  date={article.attributes.created_at}
+                  author={article.attributes.author}
+                  url="#"
+                  title={article.attributes.title}
+                  description={article.attributes.description}
+                  image={article.attributes.image_url}/>
+            )}
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Local
