f80d81552b287120f20c3a307613f216ac336182
Adrian Halaburda <adh761@gmail.com>
2021-01-09 21:59:37 -0300

    initial temp components

---
+++ blog/app/javascript/components/Post/Post.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Post = () => {
+  return(
+    <div>This is the Posts#show page for our app.</div>
+  )
+}
+
+export default Post
---
+++ blog/app/javascript/components/Posts/Posts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Posts = () => {
+  return(
+    <div>This is the Posts#INDEX page for our app.</div>
+  )
+}
+
+export default Posts
