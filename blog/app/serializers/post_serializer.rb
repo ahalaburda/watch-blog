5019c650af8992bff8ede6ea380352baa2a648a3
Adrian Halaburda <adh761@gmail.com>
2021-01-13 10:50:39 -0300

    fix:Created_at and updated_at in posts serializer

--- blog/app/serializers/post_serializer.rb
+++ blog/app/serializers/post_serializer.rb
@@ -1,4 +1,4 @@
 class PostSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :author, :description, :image_url
+  attributes :title, :author, :description, :image_url, :created_at, :updated_at
 end
