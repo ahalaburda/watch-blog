2d7659228e96ac99a3bfd0837ac2a460bb5ff8f4
Adrian Halaburda <adh761@gmail.com>
2021-01-22 15:52:36 -0300

    fix:User controller moved to the correct directory

---
+++ blog/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,43 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      protect_from_forgery with: :null_session
+      before_action :authorized, only: [:auto_login]
+
+      # SIGN UP
+      def create
+        @user = User.create(user_params)
+        if @user.valid?
+          token = encode_token({user_id: @user.id})
+          render json: {user: @user, token: token}
+        else
+          render json: {error: "Invalid username or password"}
+        end
+      end
+
+      # SIGN IN
+      def login
+        @user = User.find_by(username: params[:username])
+
+        if @user && @user.authenticate(params[:password])
+          token = encode_token({user_id: @user.id})
+          render json: {user: @user, token: token}
+        else
+          render json: {error: "Invalid username or password"}
+        end
+      end
+
+
+      def auto_login
+        render json: @user
+      end
+
+      private
+
+      def user_params
+        params.permit(:username, :password, :name)
+      end
+
+    end
+  end
+end
