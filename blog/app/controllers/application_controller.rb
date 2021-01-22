f0f5e64ce3b44643a8d74adacde540dfaa8a7af6
Adrian Halaburda <adh761@gmail.com>
2021-01-22 15:51:31 -0300

    fix: Return back to the flag base after api test

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
-# class ApplicationController < ActionController::Base
-class ApplicationController < ActionController::API
- before_action :authorized
+class ApplicationController < ActionController::Base
+ # before_action :authorized
 
   def encode_token(payload)
     JWT.encode(payload, 's3cr3t')
