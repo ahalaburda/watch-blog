8bc9e9bbf0e817b167753887bd116a7b957b60ad
Adrian Halaburda <adh761@gmail.com>
2021-01-25 12:36:18 -0300

    forgery desactivated

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
  # before_action :authorized
-
+ protect_from_forgery with: :null_session
   def encode_token(payload)
     JWT.encode(payload, 's3cr3t')
   end
